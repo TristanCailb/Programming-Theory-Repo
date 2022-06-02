5e574f66a590d14bc1d130b63296ddfd81eba379
Tristan <cailbourdint@gmail.com>
2022-06-02 16:23:54 +0200

    Added a custom cursor

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    private SpriteRenderer _rend;
+    private Camera _mainCam;
+
+    [SerializeField] private Color mouseUpColor = new Color(1f, 1f, 1f, 0.2f);
+    [SerializeField] private Color mouseDownColor = Color.white;
+
+    private void Start()
+    {
+        _rend = GetComponent<SpriteRenderer>();
+        _mainCam = Camera.main;
+        _rend.color = mouseUpColor;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        UpdatePosition();
+        UpdateColor();
+    }
+
+    /// <summary>
+    /// Update cursor position
+    /// </summary>
+    private void UpdatePosition()
+    {
+        var position = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0f;
+        transform.position = position;
+    }
+
+    /// <summary>
+    /// Update cursor color
+    /// </summary>
+    private void UpdateColor()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rend.color = mouseDownColor;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _rend.color = mouseUpColor;
+        }
+    }
+}
